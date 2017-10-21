8ff0cdc14b38a7e2d66abba5e79b9bad20da1063
Aliya Rahman <aliya.rahman@yahoo.com>
2017-10-20 21:17:12 -0400

    Added model structures

--- app/models.py
+++ app/models.py
@@ -1,5 +1,57 @@
 from __future__ import unicode_literals
+from django.utils import timezone
+from django.db import models
+import calendar
+import datetime
 
 from django.db import models
 
-# Create your models here.
+class Series(models.Model):
+    title = models.CharField(max_length = 150, null = True, blank = True)
+    partner = models.CharField(max_length = 45, default = "Wellstone Action")
+    start_date = models.CharFielmodels.DateField(null = True, blank = True)
+    end_date = models.DateField(null = True, blank = True)
+
+    def __unicode__(self):
+        return self.title
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, null=True, blank=True)
+    number = models.CharField(max_length = 3, null = True, blank=True)
+    start_date = models.DateField(null = True, blank = True)
+    end_date = models.DateField(null = True, blank = True)  
+    series = models.ForeignKey(Lesson)
+
+
+class Training(models.Model):
+    training_id = models.CharField(max_length = 3, null = True, blank = True)
+    dateandtime = models.DateTimeField(null = True, blank = True)
+    weekday = models.CharField(max_length = 10, null=True, blank=True)
+    lesson = models.ForeignKey(Lesson)
+    
+    def __unicode__(self):
+        return "Training "+ self.lesson.number+ ", "+self.weekday
+
+
+class Person(models.Model):
+    screen_name = models.CharField(max_length=20, null=True, blank=True)
+    description = models.CharField(max_length=170, null=True, blank=True)
+    location = models.CharField(max_length=150, null=True, blank=True)
+    longitude = models.FloatField(default = -93.2650)
+    latitude = models.FloatField(default = 44.9778)
+    first_name = models.CharField(max_length=45, null=True, blank=True)
+    last_name = models.CharField(max_length=45, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.screen_name
+
+
+class Message(models.Model):
+    id_str = models.CharField(max_length = 150, null=True, blank=True)
+    author = models.ForeignKey(Person)
+    text = models.TextField(null=True, blank=True)
+    time_sent = models.DateTimeField(null=True, blank=True)
+    training = models.ForeignKey(Training)
+
+    def __unicode__(self):
+        return self.person.screen_name + self.id_str
--- chat_analysis/wsgi.py
+++ chat_analysis/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chat_analysis project.
+WSGI config for wfv project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
